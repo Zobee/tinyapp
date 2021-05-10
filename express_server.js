0c8c9eeda62c559ebc1303b45253b4fead106191
Zobee <zohebboga@hotmail.com>
2021-05-10 22:06:12 +0000

    Added a POST method to create new urls

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080;
 
 app.set('view engine', "ejs");
+
+function generateRandomString() {
+  let chars = "0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+  let randomStr = "";
+  while (randomStr.length < 6){
+    let randChar = chars[Math.floor(Math.random() * chars.length)]
+    randomStr += randChar;
+  }
+  return randomStr;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,16 +34,29 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  const short = generateRandomString();
+  urlDatabase[short] = req.body.longURL
+  console.log(req.body);
+  console.log("Adding url to DB...")
+  console.log(urlDatabase)
+  res.redirect(`/urls/${short}`)       
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let url = req.params.shortURL
   const templateVars = { shortURL: url, longURL: urlDatabase[url]};
   res.render("urls_show.ejs", templateVars);
 })
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
